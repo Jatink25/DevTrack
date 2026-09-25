b8106c122630b29fad0bead4c6b1c79b77dde9bc
Jatin Kumar <nitianjatinkumar@gmail.com>
2026-09-25 12:08:42 +0530

    Fix missing process.env in refresh token expiry setting

--- Backend/src/models/User.model.js
+++ Backend/src/models/User.model.js
@@ -63,7 +63,7 @@ userSchema.methods.generateRefreshToken = function(){
     },
     process.env.REFRESH_TOKEN_SECRET,
     {
-        expiresIn:REFRESH_TOKEN_EXPIRY
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
     }
 )
 }
